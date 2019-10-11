3325ddee4fc23c1e02a2b812762dddb1b56b84ca
Valentin Babin <babin7@vafl-o-d02-06.unilim.fr>
2019-10-11 18:19:47 +0200

    init project flare

--- assets/bundle.js
+++ assets/bundle.js
@@ -1,1 +1,11 @@
 // console.log('Hello World');
+// https://www.pinterest.fr/pin/824932856718798859/
+// pseudo git/projet
+import "../assets/styles/main.scss";
+import $ from "jquery";
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+console.log($('body'));
