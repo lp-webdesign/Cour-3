366056a8503d2fe4bdbb842cd11e3d6af1d2b028
Valentin Babin <valentin.babin@hotmail.fr>
2019-10-17 23:11:55 +0200

    add header section

--- assets/bundle.js
+++ assets/bundle.js
@@ -1,11 +1,14 @@
 // console.log('Hello World');
 // https://www.pinterest.fr/pin/824932856718798859/
 // pseudo git/projet
+//https://stevenwestmoreland.com/2018/01/how-to-include-bootstrap-in-your-project-with-webpack.html
+
 import "../assets/styles/main.scss";
 import $ from "jquery";
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
+// import '../assets/styles/variables.scss';
 
 console.log($('body'));
