ffab2aae7592000efd2cb6cd8c48ebeb2d163e7c
Corentin <aazertypher@gmail.com>
2019-10-18 15:27:54 +0200

    Two

--- assets/bundle.js
+++ assets/bundle.js
@@ -1,1 +1,9 @@
 // console.log('Hello World');
+import "./styles/master.scss";
+import $ from "jquery";
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+console.log($('body'));
