74f5d2d37feb35f608046e2199818bfd9962dd92
Corentin <aazertypher@gmail.com>
2019-10-20 15:55:43 +0200

    Anim

--- assets/bundle.js
+++ assets/bundle.js
@@ -6,4 +6,30 @@ import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 
-console.log($('body'));
+$(document).ready(function() {
+    
+    $(window).scroll( function(){
+
+        $('.hidden').each( function(){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                $(this).css({"animation":"hidden 2s ease"});
+                
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
+
+$(document).ready(function() {
+    $('.hidden_document_ready').animate({'opacity':'1'},500);
+    $('.hidden_document_ready').css({"animation":"hidden 2s ease"});
+});
