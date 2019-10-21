1871aadd6d94e7f1c4874f36fa64e841865900b4
Valentin Babin <valentin.babin@hotmail.fr>
2019-10-22 00:50:09 +0200

    end html css js and add comments on css

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Maquettes : https://www.pinterest.fr/pin/824932856718798859/
+
 # Install
 
 To use Webpack run : 
--- assets/bundle.js
+++ assets/bundle.js
@@ -1,7 +1,5 @@
-// console.log('Hello World');
 // https://www.pinterest.fr/pin/824932856718798859/
 // pseudo git/projet
-//https://stevenwestmoreland.com/2018/01/how-to-include-bootstrap-in-your-project-with-webpack.html
 
 import "../assets/styles/main.scss";
 import $ from "jquery";
@@ -11,7 +9,6 @@ import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 import jQueryBridget from "jquery-bridget";
 import Flickity from "flickity";
-// import '../assets/styles/variables.scss';
 
 console.log($('body'));
 
