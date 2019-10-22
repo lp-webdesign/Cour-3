69f5a35406f096bb035ca1631254e5fd36104301
camillefarge <camillefarge.pro@gmail.com>
2019-10-22 17:42:55 +0200

    try to optimise js

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,24 @@ $('.projects__list--food').click(function() {
     $('.projects__images--food').fadeIn().addClass('projects__images--active-container').css('display', 'flex').css('justify-content', 'space-between');
     console.log('in')
 });
+
+
+
+// function projects(y){
+//         var y = this.getElementsByClassName;
+//         console.log(y)
+//         //remove the active class on li (color-primary)
+//         $('.projects__list--active').removeClass('projects__list--active');
+//         //add the active class on this li (color-primary)
+//         $(this).addClass('projects__list--active')
+//         //hide the current active images
+//         $('.projects__images--active-container').fadeOut().removeClass('projects__images--active-container');
+//         //show the new images
+//         $('.projects__images--no-active').removeClass('.projects__images--no-active')
+    
+//         y.fadeIn().addClass('projects__images--active-container').css('display', 'flex').css('justify-content', 'space-between');
+//         console.log('in')
+   
+// }
+
+// $('.projects__list--tourism').click(projects(this))
